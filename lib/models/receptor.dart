785a6c93ee5322b9780da5ca9b242cb460dff1f0
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-14 00:00:58 -0600

    Nuevo modelo receptor

---
+++ lib/models/receptor.dart
@@ -0,0 +1,11 @@
+class ContactoConfianza {
+  final int id;
+  final String telephone;
+  final String name;
+
+  const ContactoConfianza({
+    required this.id,
+    required this.telephone,
+    required this.name,
+  });
+}
