61cb5964ee998240d2e37eed016500897b18a237
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-13 23:59:58 -0600

    Contruccion de lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/lista_contactos.dart';
+
+/// Flutter code sample for [ListTile].
+
 void main() {
-  runApp(const MyApp());
+  runApp(const AppAlerta());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppAlerta extends StatelessWidget {
+  const AppAlerta({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 202, 138, 0)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  double _counter = 0.0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = _counter * _counter;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: ThemeData(useMaterial3: true),
+      home: const ListTileExample(),
     );
   }
 }
