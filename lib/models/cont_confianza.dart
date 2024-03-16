1d0ab27025e7feb2d8980c9c609cba3f366eea44
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-16 10:40:09 -0600

    Modelo de contacto de confianza

---
+++ lib/models/cont_confianza.dart
@@ -0,0 +1,26 @@
+
+class ContactoConfianza {
+  final int id;
+  final String telephone;
+  final String name;
+
+  const ContactoConfianza({
+    required this.id,
+    required this.telephone,
+    required this.name,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'telephone': telephone,
+      'name': name,
+    };
+  }
+
+  @override
+  String toString() {
+    return '{id: $id, name: $name, telephone: $telephone}';
+  }
+
+}
