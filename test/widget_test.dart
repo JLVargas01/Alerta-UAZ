54a01a844cc379840e9d12d7e4aa91e7e1f6b82a
J Luis Vargas <vargasibarra01@gmail.com>
2024-03-16 14:28:35 -0600

    Cambio en el nombre de la App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:alerta_uaz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppAlerta());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
