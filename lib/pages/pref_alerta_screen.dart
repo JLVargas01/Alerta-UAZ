bb45bd070faa7deebc0fb593c782adb667da1a6e
J Luis Vargas <vargasibarra01@gmail.com>
2024-04-01 22:03:45 -0600

    Pantalla de preferencia

--- lib/pages/pref_alerta_screen.dart
+++ lib/pages/pref_alerta_screen.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class AlertPreferrence extends StatelessWidget {
-  const AlertPreferrence({super.key});
+class AlertPreference extends StatelessWidget {
+  const AlertPreference({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Opciones de alerta')),
-        body: const  Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Tienes la opción de enviar más información en caso de una alerta con las siguientes opciones',
-                style: TextStyle(fontSize: 20),
+        body: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                'Tienes la opción de enviar más información durante una alerta con las siguientes preferencias',
+                style: TextStyle(fontSize: 18),
               ),
-              AlertPreferencePage(),
-            ],
+            ),
+            AlertPreferencePage(),
+          ],
         ),
-       ),
+      ),
     );
   }
 }
@@ -31,7 +35,9 @@ class AlertPreferencePage extends StatefulWidget {
 }
 
 class _AlertPreferencePageState extends State<AlertPreferencePage> {
-  bool light = true;
+  late SharedPreferences _prefs;
+  bool _sendAudio = false;
+  bool _sendVideo = false;
 
   final MaterialStateProperty<Icon?> thumbIcon =
       MaterialStateProperty.resolveWith<Icon?>(
@@ -44,25 +50,71 @@ class _AlertPreferencePageState extends State<AlertPreferencePage> {
   );
 
   @override
+  void initState() {
+    super.initState();
+    _initPreferences();
+  }
+
+  Future<void> _initPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _sendAudio = _prefs.getBool('sendAudio') ?? false;
+      _sendVideo = _prefs.getBool('sendVideo') ?? false;
+    });
+  }
+
+  Future<void> _changeStateAudio(bool value) async {
+    setState(() {
+      _sendAudio = value;
+    });
+    await _prefs.setBool('sendAudio', value);
+  }
+
+  Future<void> _changeStateVideo(bool value) async {
+    setState(() {
+      _sendVideo = value;
+    });
+    await _prefs.setBool('sendVideo', value);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const Text(
-          'Enviar audios grabados durante una situación de peligro',
-          style: TextStyle(fontSize: 18),
+        
+        //Primer switch para la opcion de enviar audio
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Enviar audios grabados durante una situación de peligro',
+            style: TextStyle(fontSize: 18),
           ),
+        ),
         Switch(
           thumbIcon: thumbIcon,
-          value: light,
+          value: _sendAudio,
           onChanged: (bool value) {
-            setState(() {
-              light = value;
-            });
+            _changeStateAudio(value);
+          },
+        ),
+
+        //Segundo switch para la opcion de enviar video
+        const Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            'Enviar audios grabados durante una situación de peligro',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+        Switch(
+          thumbIcon: thumbIcon,
+          value: _sendVideo,
+          onChanged: (bool value) {
+            _changeStateVideo(value);
           },
         ),
       ],
     );
   }
-
 }
