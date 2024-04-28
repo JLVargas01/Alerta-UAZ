e632ea67026258b5a4b20c039255a5e49c5c1557
Luis Adrian Hernandez <luisac658@outlook.com>
2024-04-28 01:06:11 -0600

    Se eliminó servicio shake de android

--- android/app/src/main/kotlin/com/example/alerta_uaz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/alerta_uaz/MainActivity.kt
@@ -1,14 +1,5 @@
 package com.example.alerta_uaz
 
 import io.flutter.embedding.android.FlutterActivity
-import android.content.Intent
-import android.os.Bundle
 
-class MainActivity: FlutterActivity(){
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val intent = Intent(this, ShakeDetectorService::class.java)
-        startService(intent)
-    }
-}
+class MainActivity: FlutterActivity()
