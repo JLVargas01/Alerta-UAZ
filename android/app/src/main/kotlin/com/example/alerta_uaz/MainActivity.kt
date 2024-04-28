329181fd9cc413e2074c516851594101aa1c5e17
Luis Adrian Hernandez <luisac658@outlook.com>
2024-04-27 21:43:16 -0600

    Se inicializo el servicio shake desde main de android

--- android/app/src/main/kotlin/com/example/alerta_uaz/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/alerta_uaz/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.alerta_uaz
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Inicia el ShakeDetectorService
+        Intent(this, ShakeDetectorService::class.java).also { intent ->
+            startService(intent)
+        }
+    }
+}
