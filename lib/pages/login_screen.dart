ac545ba380e7c2e653adbb3c194ea7d28c3776a5
J Luis Vargas <vargasibarra01@gmail.com>
2024-04-30 17:36:56 -0600

    Nueva pantalla estando logueado

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,19 +1,13 @@
 import 'dart:async';
 
+import 'package:alerta_uaz/api/google_sign_in_service.dart';
 import 'package:alerta_uaz/api/user_http_service.dart';
+import 'package:alerta_uaz/pages/logged_in_screen.dart';
+import 'package:alerta_uaz/services/usuario_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-const List<String> scopes = <String>[
-  'email',
-];
-
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: scopes,
-);
-
-///**************************** Clase principal
 class SignInUsuario extends StatefulWidget {
   ///
   const SignInUsuario({super.key});
@@ -23,91 +17,32 @@ class SignInUsuario extends StatefulWidget {
 }
 
 class _SignInUsuarioState extends State<SignInUsuario> {
-  GoogleSignInAccount? _currentUser; // Usuario
-  bool _isAuthorized = false; // Permisos
-
-  @override
-  void initState() {
-    super.initState();
-    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) async {
-      bool isAuthorized = account != null;
-      if (kIsWeb && account != null) {
-        isAuthorized = await _googleSignIn.canAccessScopes(scopes);
-      }
-
-      setState(() {
-        _currentUser = account;
-        _isAuthorized = isAuthorized;
-      });
-
-    });
-
-    _googleSignIn.signInSilently();
-  }
 
   Future<void> _handleSignIn() async {
-    try {
-      await _googleSignIn.signIn();
-    } catch (error) {
-      print('\n\n******************Error************************');
-      print(error);
-      print('\n\n******************Error************************');
+    final user = await GoogleSignInService.logIn();
+    if(user == null){
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Error en autenticacion'),));
+    } else {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => LoggedInPage(user: user),
+      ));
     }
   }
-
-  Future<void> _handleAuthorizeScopes() async {
-    final bool isAuthorized = await _googleSignIn.requestScopes(scopes);
-    setState(() {
-      _isAuthorized = isAuthorized;
-    });
-  }
-
-  Future<void> _handleSignOut() => _googleSignIn.disconnect();
 
   Widget _buildBody() {
-    final GoogleSignInAccount? user = _currentUser;
-    if (user != null) {
-      // The user is Authenticated
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          ListTile(
-            leading: GoogleUserCircleAvatar(
-              identity: user,
-            ),
-            title: Text(user.displayName ?? ''),
-            subtitle: Text(user.email),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        const Text('Actualmente no estás registrado.'),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            minimumSize: Size(MediaQuery.of(context).size.width / 2, 50),
           ),
-          const Text('Iniciaste sesión exitosamente.'),
-          if (!_isAuthorized) ...<Widget>[
-            const Text('Se necesitan permisos adicionales para leer sus contactos.'),
-            ElevatedButton(
-              onPressed: _handleAuthorizeScopes,
-              child: const Text('SOLICITAR PERMISOS'),
-            ),
-          ],
-          ElevatedButton(
-            onPressed: _handleSignOut,
-            child: const Text('DESCONECTAR'),
-          ),
-        ],
-      );
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          const Text('Actualmente no estás registrado.'),
-
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(MediaQuery.of(context).size.width / 2, 50),
-            ),
-            onPressed: _handleSignIn,
-            child: const Text('Iniciar sesión'),
-          ),
-        ],
-      );
-    }
+          onPressed: _handleSignIn,
+          child: const Text('Iniciar sesión'),
+        ),
+      ],
+    );
   }
 
   @override
