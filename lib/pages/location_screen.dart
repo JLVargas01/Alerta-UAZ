104cf89ea2e0e3b6d8d48f95390cce58578fa244
Luis Adrian Hernandez <luisac658@outlook.com>
2024-04-30 04:10:17 -0600

    Se actualizó location pages mostrando un mapa

--- lib/pages/location_screen.dart
+++ lib/pages/location_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+const double latitude = 40.416775;
+const double longitude = -3.703790;
+
+const LatLng location = LatLng(latitude, longitude);
 
 class LocationPage extends StatelessWidget {
   const LocationPage({super.key});
@@ -9,9 +16,23 @@ class LocationPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Localización'),
       ),
-      body: const Center(
-        child: Text('Localización'),
-      ),
+      body: FlutterMap(
+          options: const MapOptions(initialCenter: location, initialZoom: 14.0),
+          children: <Widget>[
+            TileLayer(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              //subdomains: const ['a', 'b', 'c'],
+            ),
+            const MarkerLayer(
+              markers: [
+                Marker(
+                    width: 40,
+                    height: 40,
+                    point: location,
+                    child: Icon(Icons.location_pin, color: Colors.red))
+              ],
+            ),
+          ]),
     );
   }
 }
