4320aae05e3511932a9c4e5151a5e3839b58827a
Luis Adrian Hernandez <luisac658@outlook.com>
2024-05-01 14:50:06 -0600

    Nuevo servicio socket

---
+++ lib/services/socket_service.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+class SocketService {
+  static final SocketService _instance = SocketService._internal();
+  late io.Socket _socket;
+
+  factory SocketService() {
+    return _instance;
+  }
+
+  SocketService._internal() {
+    _socket = io.io('http://${dotenv.env['API_URL']}', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    _socket.connect();
+  }
+
+  void emit(String event, String data) {
+    if (_socket.connected) {
+      _socket.emit(event, data);
+    } else {
+      print('Socket is not connected');
+    }
+  }
+
+  void dispose() {
+    _socket.disconnect();
+  }
+}
