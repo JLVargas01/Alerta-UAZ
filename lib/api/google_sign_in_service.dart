7efe20bce01b1c2c325a4741c2acb897bdf0e210
J Luis Vargas <vargasibarra01@gmail.com>
2024-04-30 18:51:49 -0600

    Servicios de google logueo y deslogueo

---
+++ lib/api/google_sign_in_service.dart
@@ -0,0 +1,11 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSignInService {
+
+  static final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  static Future<GoogleSignInAccount?> logIn() => _googleSignIn.signIn();
+
+  static Future<void> logOut() => _googleSignIn.disconnect();
+
+}
