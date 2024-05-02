bf9fd67a32994cd449d2fa31821326bd58bda783
Luis Adrian Hernandez <luisac658@outlook.com>
2024-05-02 15:19:06 -0600

    Inicialización de servicios en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,35 @@
+import 'package:alerta_uaz/pages/alert_screen.dart';
 import 'package:alerta_uaz/pages/contacts_screen.dart';
+import 'package:alerta_uaz/services/api_service.dart';
+import 'package:alerta_uaz/services/push_notification_service.dart';
+import 'package:alerta_uaz/services/shake_detector_service.dart';
+import 'package:alerta_uaz/services/socket_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  socket.initialize();
+
+  PushNotificationService.initialize();
+
+  ShakeDetectorService.startListening(() async {
+    await apiService.sendNotification('4921231234');
+    ShakeDetectorService.pauseListening();
+
+    navigatorKey.currentState
+        ?.pushReplacement(MaterialPageRoute(builder: (_) => const AlertPage()));
+  });
+
   runApp(const AppAlerta());
 }
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+ApiService apiService = ApiService();
+final socket = SocketService();
 
 class AppAlerta extends StatelessWidget {
   const AppAlerta({super.key});
@@ -14,6 +38,7 @@ class AppAlerta extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
+      navigatorKey: navigatorKey,
       home: const ContactosPage(),
     );
   }
