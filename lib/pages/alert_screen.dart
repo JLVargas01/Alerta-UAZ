87721cf0905edab585464abd8fff7310626b3b0d
Luis Adrian Hernandez <luisac658@outlook.com>
2024-05-12 18:29:15 -0600

    Actualizción de pages alert y location

--- lib/pages/alert_screen.dart
+++ lib/pages/alert_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
-
 import 'package:alerta_uaz/main.dart';
-import 'package:alerta_uaz/pages/contacts_screen.dart';
-import 'package:alerta_uaz/services/location_service.dart';
-import 'package:alerta_uaz/services/shake_detector_service.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:alerta_uaz/services/location_service.dart';
+import 'package:alerta_uaz/services/shake_detector_service.dart';
+import 'package:alerta_uaz/services/socket_service.dart';
+import 'package:alerta_uaz/pages/contacts_screen.dart';
 
 class AlertPage extends StatefulWidget {
   const AlertPage({super.key});
@@ -16,38 +16,82 @@ class AlertPage extends StatefulWidget {
 
 class _AlertPageState extends State<AlertPage> {
   Timer? _timer;
-  LocationService locationService = LocationService();
-
-  Map<String, dynamic> positionToJson(Position position) {
-    return {'latitude': position.latitude, 'longitude': position.longitude};
-  }
-
-  void sendLocation() {
-    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
-      try {
-        Position position = await locationService.getCurrentLocation();
-        socket.emit('location', positionToJson(position));
-      } catch (e) {
-        print('Error: $e');
-      }
-    });
-  }
+  final LocationService locationService = LocationService();
+  final SocketService socketService = SocketService();
+  String errorMessage = '';
+  bool isOnline = true;
 
   @override
   void initState() {
     super.initState();
-    sendLocation();
+    socketService.onStatusChanged =
+        _handleConnectionChange; // Escucha cambios de estado de conexión
+    sendLocation(); // Inicia el envío periódico de la ubicación
+  }
+
+  void _handleConnectionChange(bool connected, String message) {
+    setState(() {
+      isOnline = connected;
+      errorMessage = message;
+    });
+    if (connected) {
+      sendLocation(); // Reinicia el envío de ubicación al reconectar
+    } else {
+      _timer?.cancel(); // Detiene el envío si se pierde la conexión
+    }
+  }
+
+  void sendLocation() {
+    _timer?.cancel(); // Previene la creación de múltiples timers
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+      try {
+        if (!socketService.isConnected()) {
+          // throw Exception('No hay conexión a internet.');
+          errorMessage = 'No hay conexión a internet.';
+          return;
+        }
+        Position position = await locationService.getCurrentLocation();
+        socketService.emit('location',
+            {'latitude': position.latitude, 'longitude': position.longitude});
+      } catch (e) {
+        setState(() {
+          errorMessage = e.toString();
+          isOnline = false;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text('Alerta Activada'),
-        ),
+        title: const Center(child: Text('Alerta Activada')),
       ),
-      body: Center(
+      body: Column(
+        children: [
+          _buildConnectionErrorBar(),
+          _buildContent(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildConnectionErrorBar() {
+    return isOnline
+        ? const SizedBox.shrink()
+        : Container(
+            color: Colors.red,
+            width: double.infinity,
+            padding: const EdgeInsets.all(8),
+            child:
+                Text(errorMessage, style: const TextStyle(color: Colors.white)),
+          );
+  }
+
+  Widget _buildContent() {
+    return Expanded(
+      child: Center(
         child: GestureDetector(
           onTap: () {
             ShakeDetectorService.resumeListening();
@@ -60,10 +104,8 @@ class _AlertPageState extends State<AlertPage> {
             decoration:
                 BoxDecoration(color: Colors.blue[300], shape: BoxShape.circle),
             child: const Center(
-              child: Text(
-                'Desactivar alerta',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-              ),
+              child: Text('Desactivar alerta',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900)),
             ),
           ),
         ),
@@ -74,6 +116,8 @@ class _AlertPageState extends State<AlertPage> {
   @override
   void dispose() {
     _timer?.cancel();
+    socketService
+        .dispose(); // Asegura que los recursos del servicio de socket se liberan correctamente
     super.dispose();
   }
 }
