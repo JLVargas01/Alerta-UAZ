71a4774c3de91b1812023c5fa30b63321aca46cd
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-10 00:23:18 -0600

    Nuevas pages de prueba

---
+++ lib/presentation/pages/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:alerta_uaz/application/authentication/auth_cubic.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Iniciar Sesión con Google'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            BlocProvider.of<UserBloc>(context).add(SignIn());
+          },
+          child: const Text('Iniciar sesión con Google'),
+        ),
+      ),
+    );
+  }
+}
