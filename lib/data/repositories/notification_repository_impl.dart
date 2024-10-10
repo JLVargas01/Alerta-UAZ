32b5b8f120b3c0621d11c6341d5c1e4e40ec7fac
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-10 16:49:18 -0600

    Se actualizó la implementación de interfaz notificaciones

--- lib/data/repositories/notification_repository_impl.dart
+++ lib/data/repositories/notification_repository_impl.dart
@@ -1,29 +1,14 @@
-import 'dart:convert';
-
+import 'package:alerta_uaz/data/data_sources/remote/notification_service.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-import 'package:http/http.dart' as http;
-
 import '../data_sources/remote/firebase_messaging_service.dart';
 import '../../domain/repositories/notifications_repository.dart';
 
 class NotificationsRepositoryImpl implements NotificationRepository {
   final FirebaseMessagingService _firebaseMessagingService;
+  final NotificationService _notificationService;
 
-  final String? protocol = dotenv.env['PROTOCOL'];
-  final String? hostname = dotenv.env['HOST_NAME'];
-  final String? port = dotenv.env['PORT_NOTIFICATION'];
-
-  String? _baseURL;
-
-  NotificationsRepositoryImpl(this._firebaseMessagingService) {
-    if (protocol == null || hostname == null || port == null) {
-      throw Exception(
-          'Error: Asegúrate de que PROTOCOL, HOST_NAME y PORT_NOTIFICATION están definidos.');
-    }
-    _baseURL = '$protocol://$hostname:$port';
-  }
+  NotificationsRepositoryImpl(
+      this._firebaseMessagingService, this._notificationService);
 
   @override
   Stream<RemoteMessage> getNotificationStream() {
@@ -50,34 +35,8 @@ class NotificationsRepositoryImpl implements NotificationRepository {
     await _firebaseMessagingService.deleteDeviceToken();
   }
 
+  @override
   Future<void> notificationAlert() async {
-    // Lógica para enviar notificaciones de alerta a contactos
-    const endponint = '';
-    Map<String, Object> data = {};
-    return await _sendNotification(endponint, data);
-  }
-
-  Future<void> _sendNotification(
-      String endpoint, Map<String, Object> data) async {
-    if (_baseURL == null) {
-      throw Exception('Error: Base URL no está configurada correctamente.');
-    }
-
-    final url = Uri.parse(_baseURL! + endpoint);
-
-    try {
-      final response = await http.post(
-        url,
-        headers: <String, String>{'Content-Type': 'application/json'},
-        body: json.encode(data),
-      );
-
-      if (response.statusCode != 200) {
-        throw Exception(
-            'Error al enviar los datos $endpoint: ${response.body}');
-      }
-    } catch (e) {
-      throw Exception('Error al enviar la notificación: $e');
-    }
+    await _notificationService.notificationAlert();
   }
 }
