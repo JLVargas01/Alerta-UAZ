041dbbbc70cc1eec56d072f54af62edeb4d345d7
jlvargas01 <vargasibarra01@gmail.com>
2024-10-17 14:22:16 -0600

    Correcion de nombre

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:alerta_uaz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AppAlerta());
+    await tester.pumpWidget(const AppAlert());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
