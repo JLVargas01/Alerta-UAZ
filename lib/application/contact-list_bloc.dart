437bcba162a312faf71de6b531ca3a955518897c
jlvargas01 <vargasibarra01@gmail.com>
2024-10-19 21:05:35 -0600

    Arreglado la obtencion del id la la lista de contactos

--- lib/application/contact-list_bloc.dart
+++ lib/application/contact-list_bloc.dart
@@ -1,4 +1,8 @@
+import 'package:alerta_uaz/data/data_sources/local/user_storange.dart';
+import 'package:alerta_uaz/data/data_sources/remote/user_service.dart';
+import 'package:alerta_uaz/data/repositories/contacts_repository_imp.dart';
 import 'package:alerta_uaz/domain/model/cont-confianza_model.dart';
+import 'package:alerta_uaz/domain/model/user_model.dart';
 import 'package:alerta_uaz/services/contacts_db.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttercontactpicker/fluttercontactpicker.dart';
@@ -10,7 +14,7 @@ class LoadContacts extends ContactsEvent {}
 class AddContact extends ContactsEvent {}
 
 class RemoveContact extends ContactsEvent {
-  final int id_confianza;
+  final String id_confianza;
   RemoveContact(this.id_confianza);
 }
 
@@ -32,7 +36,8 @@ class ContactsError extends ContactsState {
 
 class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
   final contacsDB = ContactosConfianza();
-  Future<List<ContactoConfianza>>? futureContcs;
+  final UserStorange _userStorange = UserStorange();
+  final ContactsRepositoryImpl _contactsRepositoryImpl = ContactsRepositoryImpl(UserService());
 
   ContactsBloc() : super(ContactsInitial()) {
     on<LoadContacts>((event, emit) async {
@@ -51,25 +56,25 @@ class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
         //Tomar los datos del contacto seleccionado
         final PhoneContact contactPicker = await FlutterContactPicker.pickPhoneContact();
         String numeroTelefonico = contactPicker.phoneNumber?.number ?? '';
-        String nombreCompleto = contactPicker.fullName ?? '';
+        String nombre = contactPicker.fullName ?? '';
 
-        /*
-        //
-        //Hacer un registro en el servidor
-        //TODO
-        // Tomar el id del registro del usuario para crear el usuario
-        //
-         */
+        User? user = await _userStorange.getUser();
+        String? idLista = user?.idContacts;
 
-        //Hacer el registro en la db local
-        ContactoConfianza nuevoContacto = ContactoConfianza(
-          id_confianza: 1,  //Este id es temporal, no funcionara mas de una vez
-          alias: nombreCompleto,  //ok
-          telephone: numeroTelefonico,  //ok
-          relacion: '', //Este dato es temporal, se debe desarrollar algo para obtenerlo
+        String? idNewContact = await _contactsRepositoryImpl.createContact(nombre, numeroTelefonico, idLista!);
+        if(idNewContact != null) {
+          //Hacer el registro en la db local
+          ContactoConfianza nuevoContacto = ContactoConfianza(
+            id_confianza: idNewContact,
+            alias: nombre,
+            telephone: numeroTelefonico,
           );
-        await contacsDB.insertContacto(nuevoContacto);
 
+          await contacsDB.insertContacto(nuevoContacto);
+        } else {
+          emit(ContactsError(
+              'Error al iniciar sesión, por favor intentelo más tarde'));
+        }
         // Recargamos la lista de contactos
         final contactos = await contacsDB.contactos();
         emit(ContactsLoaded(contactos));
