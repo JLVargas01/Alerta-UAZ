dc93e736fd9926c2ddc9236640bb80b064902122
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-22 14:48:24 -0600

    Nuevo path de bloc authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alerta_uaz/application/authentication/auth_bloc.dart';
+import 'package:alerta_uaz/application/auth_bloc.dart';
 import 'package:alerta_uaz/application/contact-list_bloc.dart';
 import 'package:alerta_uaz/presentation/routes.dart';
 import 'package:flutter/material.dart';
@@ -15,13 +15,11 @@ Future<void> main() async {
       providers: [
         // Verifica si ya está autenticado
         BlocProvider(
-          create: (context) => AuthBloc()
-            ..add(CheckUserAuthentication()),
+          create: (context) => AuthBloc()..add(CheckUserAuthentication()),
         ),
         // Cargar contactos guardados
         BlocProvider(
-          create: (context) => ContactsBloc()
-            ..add(LoadContacts()),
+          create: (context) => ContactsBloc()..add(LoadContacts()),
         ),
       ],
       child: const AppAlert(),
--- lib/presentation/pages/profile_page.dart
+++ lib/presentation/pages/profile_page.dart
@@ -1,4 +1,4 @@
-import 'package:alerta_uaz/application/authentication/auth_bloc.dart';
+import 'package:alerta_uaz/application/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
