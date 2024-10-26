0744c187d0690544e5b3aa83ffd5f10ebace605e
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-26 00:40:04 -0600

    Nueva ruta para el mapa

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -1,8 +1,10 @@
 import 'package:alerta_uaz/presentation/pages/login_page.dart';
 import 'package:alerta_uaz/presentation/pages/main_page.dart';
+import 'package:alerta_uaz/presentation/pages/map_page.dart';
 import 'package:flutter/widgets.dart';
 
 Map<String, WidgetBuilder> routes = {
   "/login": (_) => const LoginPage(),
   "/main": (_) => const MainPage(),
+  "/map": (_) => const MapPage(),
 };
