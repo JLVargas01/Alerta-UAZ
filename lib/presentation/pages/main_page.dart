895dd4d1d5b08d962ea8c7a47721c122f51ebfb2
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-26 00:42:31 -0600

    Actualización en main page para mostrar dialogo de notificación

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,11 +1,32 @@
+import 'package:alerta_uaz/application/notification_bloc.dart';
+import 'package:alerta_uaz/domain/model/notification_model.dart';
 import 'package:alerta_uaz/presentation/widget/indexed_stack_navigation.dart';
+import 'package:alerta_uaz/presentation/widget/notification_alert.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const IndexedStackNavigation();
+    return BlocListener<NotificationBloc, NotificationState>(
+      listener: (context, state) {
+        if (state is NotificationReceived) {
+          _showNotificationAlert(context, state.notificationModel);
+        }
+      },
+      child: const IndexedStackNavigation(),
+    );
+  }
+
+  void _showNotificationAlert(
+      BuildContext context, NotificationModel notification) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return NotificationAlert(notificationModel: notification);
+      },
+    );
   }
 }
