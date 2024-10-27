c1fb8330f9f810828cd351b7ad3a0cde472a5027
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-27 02:58:50 -0600

    Actualización en user service

--- lib/data/data_sources/remote/user_service.dart
+++ lib/data/data_sources/remote/user_service.dart
@@ -1,73 +1,49 @@
 import 'dart:convert';
+import 'package:alerta_uaz/core/constants/api_config.dart';
+import 'package:alerta_uaz/core/utils/http_helper.dart';
 import 'package:alerta_uaz/domain/model/user_model.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
 class UserService {
-  final String? _protocol = dotenv.env['PROTOCOL'];
-  final String? _hostname = dotenv.env['HOST_NAME'];
-  final String? _port = dotenv.env['PORT_USER'];
-
-  String? _baseURL;
-
-  UserService() {
-    if (_protocol == null || _hostname == null || _port == null) {
-      throw Exception(
-          'Error: Asegúrate de que PROTOCOL, HOST_NAME y PORT_USER están definidos.');
-    }
-    _baseURL = '$_protocol://$_hostname:$_port';
-  }
+  final String _baseUrl = ApiConfig.getBaseUrl(ApiConfig.portUser);
 
   Future<User?> signInUser(User user) async {
-    String? endpoint = dotenv.env['USER_SIGN_IN'];
+    String endpoint = ApiConfig.singIn;
 
-    Uri uri = Uri.parse('$_baseURL$endpoint');
+    try {
+      Uri uri = Uri.parse('$_baseUrl$endpoint');
 
-    Map<String, dynamic> data = user.toJson();
+      Map<String, dynamic> data = user.toJson();
 
-    final response = await _sendData(data, uri);
+      final response = await HttpHelper.post(uri, data);
 
-    if (response.statusCode == 201 || response.statusCode == 200) {
-      final Map<String, dynamic> responseData = json.decode(response.body);
-      return User.fromJson(responseData);
-    } else {
-      return null;
+      data = json.decode(response.body);
+
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        return User.fromJson(data);
+      } else {
+        throw Exception('${data['error']}');
+      }
+    } catch (e) {
+      throw Exception('Error de red: $e');
     }
   }
 
-  Future<String?> sendDataNewContact(String nombre, String telefono, String idLista) async {
-    String? endpoint = dotenv.env['USER_CREATE_CONTACT'];
+  Future<String?> sendDataNewContact(
+      String nombre, String telefono, String idLista) async {
+    String endpoint = ApiConfig.createContact;
 
-    Uri uri = Uri.parse('$_baseURL$endpoint');
+    Uri uri = Uri.parse('$_baseUrl$endpoint');
 
     Map<String, dynamic> parametersSend = {
       'id_listConta': idLista,
       'alias': nombre,
       'telephone': telefono
     };
-    final response = await _sendData(parametersSend, uri);
+    final response = await HttpHelper.post(uri, parametersSend);
     if (response.statusCode == 201) {
       return response.body;
     } else {
       return null;
     }
-  }
-
-
-  Future<http.Response> _sendData(Map<String, dynamic> data, Uri uri) async {
-    const headerKey = "********************************************";
-
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Authorization': headerKey,
-    };
-
-    http.Response response = await http.post(
-      uri,
-      headers: headers,
-      body: jsonEncode(data),
-    );
-
-    return response;
   }
 }
