317067146a5b3a2c93388eeb993d439df5f26e92
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-27 03:53:13 -0600

    Nueva ruta para alert page

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -1,9 +1,11 @@
+import 'package:alerta_uaz/presentation/pages/alert_page.dart';
 import 'package:alerta_uaz/presentation/pages/login_page.dart';
 import 'package:alerta_uaz/presentation/pages/main_page.dart';
 import 'package:alerta_uaz/presentation/pages/map_page.dart';
 import 'package:flutter/widgets.dart';
 
 Map<String, WidgetBuilder> routes = {
+  "/alert": (_) => const AlertPage(),
   "/login": (_) => const LoginPage(),
   "/main": (_) => const MainPage(),
   "/map": (_) => const MapPage(),
