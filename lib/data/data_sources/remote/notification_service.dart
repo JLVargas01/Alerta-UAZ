f6548789ced434aa31d755a469642f2694f1aba1
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-27 01:18:53 -0600

    Uso de funciones y constantes globales en notification service

--- lib/data/data_sources/remote/notification_service.dart
+++ lib/data/data_sources/remote/notification_service.dart
@@ -1,52 +1,43 @@
 import 'dart:convert';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:alerta_uaz/core/constants/api_config.dart';
+import 'package:alerta_uaz/core/utils/http_helper.dart';
 import 'package:http/http.dart' as http;
 
 class NotificationService {
-  final String? protocol = dotenv.env['PROTOCOL'];
-  final String? hostname = dotenv.env['HOST_NAME'];
-  final String? port = dotenv.env['PORT_NOTIFICATION'];
+  final String _baseUrl = ApiConfig.getBaseUrl(ApiConfig.portNotification);
 
-  String? _baseURL;
+  Future<String> notificationAlert(
+      String idContact, Map<String, dynamic> data) async {
+    String? endpoint = '${ApiConfig.notificationAlert}/$idContact';
 
-  NotificationService() {
-    if (protocol == null || hostname == null || port == null) {
-      throw Exception(
-          'Error: Asegúrate de que PROTOCOL, HOST_NAME y PORT_NOTIFICATION están definidos.');
-    }
-    _baseURL = '$protocol://$hostname:$port';
+    Map<String, Object> message = {
+      'notification': {
+        'title': '¡Alerta de emergencia!',
+        'body':
+            '${data['name']} necesita ayuda urgente. Se encuentra en una situación de peligro.'
+      },
+      'data': data
+    };
+
+    return await _sendNotification(endpoint, message);
   }
 
-  Future<void> notificationAlert() async {
-    // Lógica para enviar notificaciones de alerta a contactos
-    const endponint = '';
-    Map<String, Object> data = {};
-    return await _sendNotification(endponint, data);
-  }
-
-  Future<void> _sendNotification(
-      String endpoint, Map<String, Object> data) async {
-    if (_baseURL == null) {
-      throw Exception('Error: Base URL no está configurada correctamente.');
-    }
-
-    final url = Uri.parse(_baseURL! + endpoint);
+  Future<String> _sendNotification(
+      String endpoint, Map<String, dynamic> data) async {
+    final Uri url = Uri.parse(_baseUrl + endpoint);
 
     try {
-      final response = await http.post(
-        url,
-        headers: <String, String>{'Content-Type': 'application/json'},
-        body: json.encode(data),
-      );
+      http.Response response = await HttpHelper.post(url, data);
+
+      data = json.decode(response.body);
 
       if (response.statusCode != 200) {
-        throw Exception(
-            'Error al enviar los datos $endpoint: ${response.body}');
+        throw Exception(data['error'].toString());
       }
+      return data['success'].toString();
     } catch (e) {
-      throw Exception('Error al enviar la notificación: $e');
+      throw Exception('Error de red: $e');
     }
   }
 }
