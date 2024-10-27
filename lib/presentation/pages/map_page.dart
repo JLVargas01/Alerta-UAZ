1b11d8c41b07fd452c5ee30194f55c031c49df7e
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-27 03:54:54 -0600

    Se movió los listeners de map page

--- lib/presentation/pages/map_page.dart
+++ lib/presentation/pages/map_page.dart
@@ -1,8 +1,4 @@
-import 'package:alerta_uaz/application/alert/alert_bloc.dart';
-import 'package:alerta_uaz/application/alert/alert_state.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -15,44 +11,25 @@ class MapPage extends StatelessWidget {
     final Map<String, dynamic> data =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
 
-    // Utiliza 'message' según tus necesidades
     return Scaffold(
         appBar: AppBar(
           title: const Text('Mapa'),
           automaticallyImplyLeading: false,
         ),
-        body: BlocListener<AlertBloc, AlertState>(
-          listener: (context, state) {
-            if (state is AlertSent) {
-              // mensaje de notificación enviada a contactos
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(state.success)));
-            } else if (state is AlertSending) {
-              // mensaje de notificaicón enviandose
-              ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Enviando alerta...')));
-            } else if (state is AlertError) {
-              // mensaje de error al enviar notificación
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(state.error)));
-            }
-          },
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Datos de la notificación: ${data['name']}'),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      context.read<AlertBloc>().resumeListeningShake();
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Regresar'))
-              ],
-            ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Datos de la notificación: ${data['name']}'),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Regresar'))
+            ],
           ),
         ));
   }
