1cfb02b83e1946153db46218d42750e32c8cc732
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-27 01:25:39 -0600

    Uso de bloc Alert en map page

--- lib/presentation/pages/map_page.dart
+++ lib/presentation/pages/map_page.dart
@@ -1,4 +1,6 @@
+import 'package:alerta_uaz/application/alert_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -13,14 +15,43 @@ class MapPage extends StatelessWidget {
 
     // Utiliza 'message' según tus necesidades
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mapa'),
-      ),
-      body: Center(
-        child: Text(
-          'Datos de la notificación: ${data['message']}',
+        appBar: AppBar(
+          title: const Text('Mapa'),
+          automaticallyImplyLeading: false,
         ),
-      ),
-    );
+        body: BlocListener<AlertBloc, AlertState>(
+          listener: (context, state) {
+            if (state is AlertSent) {
+              // mensaje de notificación enviada a contactos
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(state.success)));
+            } else if (state is AlertSending) {
+              // mensaje de notificaicón enviandose
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Enviando alerta...')));
+            } else if (state is AlertError) {
+              // mensaje de error al enviar notificación
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(state.error)));
+            }
+          },
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Datos de la notificación: ${data['name']}'),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      context.read<AlertBloc>().resumeListeningShake();
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Regresar'))
+              ],
+            ),
+          ),
+        ));
   }
 }
