26242e349e82930fca58abfd0ae53524019d6868
jlvargas01 <vargasibarra01@gmail.com>
2024-11-06 19:31:51 -0600

    Nueva ruta hacia la pagina para solicitar el numero

--- lib/presentation/routes.dart
+++ lib/presentation/routes.dart
@@ -2,11 +2,13 @@ import 'package:alerta_uaz/presentation/pages/alert_page.dart';
 import 'package:alerta_uaz/presentation/pages/login_page.dart';
 import 'package:alerta_uaz/presentation/pages/main_page.dart';
 import 'package:alerta_uaz/presentation/pages/map_page.dart';
+import 'package:alerta_uaz/presentation/pages/request_phone_page.dart';
 import 'package:flutter/widgets.dart';
 
 Map<String, WidgetBuilder> routes = {
   "/alert": (_) => const AlertPage(),
   "/login": (_) => const LoginPage(),
+  "/requestPhone": (_) => const RequestPhonePage(),
   "/main": (_) => const MainPage(),
   "/map": (_) => const MapPage(),
 };
