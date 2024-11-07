cb0b7cb396e5344682a460c3ab602dbc303576eb
jlvargas01 <vargasibarra01@gmail.com>
2024-11-06 19:30:25 -0600

    Modificaion de evento para resivir numero telefonico opcional

--- lib/application/authentication/auth_event.dart
+++ lib/application/authentication/auth_event.dart
@@ -2,6 +2,10 @@ abstract class AuthEvent {}
 
 class CheckUserAuthentication extends AuthEvent {}
 
-class SignIn extends AuthEvent {}
+class SignIn extends AuthEvent {
+  final String? phoneNumber;
+  SignIn([this.phoneNumber]);
+}
+
 
 class SignOut extends AuthEvent {}
