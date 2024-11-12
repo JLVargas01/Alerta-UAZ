cb8b92e45533d7affefb42021a841b7a81241abe
jlvargas01 <vargasibarra01@gmail.com>
2024-11-12 13:02:29 -0600

    Modificacion de clase User

--- lib/domain/model/user_model.dart
+++ lib/domain/model/user_model.dart
@@ -1,16 +1,29 @@
 class User {
+  String _id;
+  String _name;
+  String _email;
+  String _phone;
+  String _avatar;
+  String _deviceToken;
+  String _idContactList;
 
-  User({String? id, String? name, String? email, String? phone, String? deviceToken, String? avatar, String? idContactList});
+  User({
+    String? id,
+    String? name,
+    String? email,
+    String? phone,
+    String? deviceToken,
+    String? avatar,
+    String? idContactList,
+  })  : _id = id ?? '',
+        _name = name ?? '',
+        _email = email ?? '',
+        _phone = phone ?? '',
+        _avatar = avatar ?? '',
+        _deviceToken = deviceToken ?? '',
+        _idContactList = idContactList ?? '';
 
-  late String _id;
-  late String _name;
-  late String _email;
-  late String _phone;
-  late String _avatar;
-  late String _deviceToken;
-  late String _idContactList;
-
-  // getters
+  // Getters
   String get id => _id;
   String get name => _name;
   String get email => _email;
@@ -19,29 +32,16 @@ class User {
   String get token => _deviceToken;
   String get idContacts => _idContactList;
 
-  // setters
-  set deviceId(String id) {
-    _id = id;
-  }
-  set deviceName(String name) {
-    _name = name;
-  }
-  set deviceEmail(String email) {
-    _email = email;
-  }
-  set devicePhone(String phone) {
-    _phone = phone;
-  }
-  set deviceAvatar(String avatar) {
-    _avatar = avatar;
-  }
-  set deviceToken(String token) {
-    _deviceToken = token;
-  }
-  set deviceIdContacts(String idContacts) {
-    _idContactList = idContacts;
-  }
+  // Setters
+  set id(String id) => _id = id;
+  set name(String name) => _name = name;
+  set email(String email) => _email = email;
+  set phone(String phone) => _phone = phone;
+  set avatar(String avatar) => _avatar = avatar;
+  set token(String token) => _deviceToken = token;
+  set idContacts(String idContacts) => _idContactList = idContacts;
 
+  // Método para serializar a JSON
   Map<String, dynamic> toJson() {
     return {
       'id': _id,
@@ -50,23 +50,24 @@ class User {
       'phone': _phone,
       'avatar': _avatar,
       'token': _deviceToken,
-      'id_contact_list': _idContactList
+      'id_contact_list': _idContactList,
     };
   }
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-        id: json['id'],
-        name: json['name'],
-        email: json['email'],
-        phone: json['phone'],
-        avatar: json['avatar'],
-        deviceToken: json['token'],
-        idContactList: json['id_contact_list']);
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+      avatar: json['avatar'] ?? '',
+      deviceToken: json['token'] ?? '',
+      idContactList: json['id_contact_list'] ?? '',
+    );
   }
 
   @override
   String toString() {
-    return '$_name,\n $_email';
+    return '$_id $_name,\n $_email,\n $_phone,\n $_avatar,\n $_deviceToken,\n $_idContactList';
   }
 }
