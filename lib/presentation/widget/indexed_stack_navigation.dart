1e2bb1c8335cb06eab1b611bac707b6e17443eef
jlvargas01 <vargasibarra01@gmail.com>
2024-11-19 23:01:01 -0600

    Mod de apariencia y agregado la pagina del historial

--- lib/presentation/widget/indexed_stack_navigation.dart
+++ lib/presentation/widget/indexed_stack_navigation.dart
@@ -1,3 +1,4 @@
+import 'package:alerta_uaz/presentation/pages/alert_history_page.dart';
 import 'package:alerta_uaz/presentation/pages/contact_page.dart';
 import 'package:alerta_uaz/presentation/pages/profile_page.dart';
 import 'package:alerta_uaz/presentation/pages/setting_page.dart';
@@ -13,6 +14,12 @@ class IndexedStackNavigation extends StatefulWidget {
 class _IndexedStackNavigationState extends State<IndexedStackNavigation> {
   int _selectedIndex = 0;
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,29 +28,25 @@ class _IndexedStackNavigationState extends State<IndexedStackNavigation> {
         children: const [
           ProfilePage(),
           ContactPage(),
+          AlertHistoryPage(),
           SettingPage(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        selectedItemColor: Colors.black, // Colores para íconos seleccionados
+        unselectedItemColor: Colors.grey, // Colores para íconos no seleccionados
+        backgroundColor: Colors.white, // Fondo
+        selectedLabelStyle: const TextStyle(fontSize: 15),
+        unselectedLabelStyle: const TextStyle(fontSize: 15),
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.contacts), label: 'Contactos'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Configuración')
+          BottomNavigationBarItem(icon: Icon(Icons.person, size: 24), label: 'Perfil'),
+          BottomNavigationBarItem(icon: Icon(Icons.contacts, size: 24), label: 'Contactos'),
+          BottomNavigationBarItem(icon: Icon(Icons.history, size: 24), label: 'Historial'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings, size: 24), label: 'Configuración'),
         ],
       ),
     );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }
