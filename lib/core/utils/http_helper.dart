da6044a3d2e1ac944e0870b99a2a810a612daa9b
jlvargas01 <vargasibarra01@gmail.com>
2024-11-25 17:20:00 -0600

    metodo get y manejo de errores en metodos http

--- lib/core/utils/http_helper.dart
+++ lib/core/utils/http_helper.dart
@@ -4,22 +4,45 @@ import 'package:alerta_uaz/core/constants/api_config.dart';
 import 'package:http/http.dart' as http;
 
 class HttpHelper {
+  static Map<String, String> _defaultHeaders() {
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': ApiConfig.headerKey,
+    };
+  }
+
   static Future<http.Response> post(Uri url, Map<String, dynamic> data) async {
-    return await http.post(url,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': ApiConfig.headerKey
-        },
-        body: jsonEncode(data));
+    try {
+      return await http.post(
+        url,
+        headers: _defaultHeaders(),
+        body: jsonEncode(data)
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  static Future<http.Response> get(Uri url) async {
+    try {
+      return await http.get(
+        url,
+        headers: _defaultHeaders()
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 
   static Future<http.Response> delete(Uri url) async {
-    return await http.delete(url,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': ApiConfig.headerKey
-      }
-    );
+    try {
+      return await http.delete(
+        url,
+        headers: _defaultHeaders()
+      );
+    } catch (e) {
+      rethrow;
+    }
   }
 
 }
