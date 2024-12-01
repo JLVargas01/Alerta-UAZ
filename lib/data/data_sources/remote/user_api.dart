ff8b399fe6884bebb3eb32b08558fa7f436f68ba
Luis Adrian Hernandez <luisac658@outlook.com>
2024-11-30 23:57:54 -0600

    Parches realizados para autenticación

--- lib/data/data_sources/remote/user_api.dart
+++ lib/data/data_sources/remote/user_api.dart
@@ -7,7 +7,8 @@ import 'package:http/http.dart' as http;
 class UserApi {
   final String _baseUrl = ApiConfig.getBaseUrl(ApiConfig.portUser, 'user');
 
-  Future<Map<String, dynamic>> signIn(Map<String, dynamic> data) async {
+  // Función que crea y registra un usuario desde el servidor con los datos enviados.
+  Future<Map<String, dynamic>?> create(Map<String, dynamic> data) async {
     const endpoint = '/create';
     final uri = Uri.parse('$_baseUrl$endpoint');
 
@@ -18,32 +19,53 @@ class UserApi {
 
       if (response.statusCode == 201) {
         return data; // El usuario ha sido registrado.
+      } else if (response.statusCode == 409) {
+        return null; // Usuario ya existe.
       } else {
-        throw data['message']; // Hubo un error al crear el usuario.
+        throw 'Hay problemas en el servidor, por favor intenelo más tarde.';
       }
     } catch (error) {
-      throw error.toString();
+      rethrow;
     }
   }
 
-  Future<Map<String, dynamic>> logIn(String email) async {
+  // Función que inicia sesión del usuario verificando correo existente en el servidor.
+  Future<Map<String, dynamic>?> getUserByEmail(String email) async {
     final endpoint = '/byEmail/$email';
 
     final uri = Uri.parse('$_baseUrl$endpoint');
 
-    Map<String, dynamic> data = {};
-
     try {
-      http.Response response = await HttpHelper.post(uri, data);
-      data = json.decode(response.body);
+      final response = await HttpHelper.get(uri);
+      final data = json.decode(response.body);
 
       if (response.statusCode == 200) {
-        return data;
+        return data; // Encontró el usuario.
+      } else if (response.statusCode == 404) {
+        return null; // El usuario no existe.
       } else {
-        throw data['message'];
+        throw 'Hay problemas en el servidor, por favor intenelo más tarde.';
       }
     } catch (error) {
-      throw error.toString();
+      rethrow;
+    }
+  }
+
+  // Función para actualizar token del usuario.
+  Future<void> updateToken(String userId, Map<String, dynamic> data) async {
+    final endpoint = '/byId/$userId/token';
+
+    final uri = Uri.parse('$_baseUrl$endpoint');
+
+    try {
+      http.Response response = await HttpHelper.patch(uri, data);
+      data = json.decode(response.body);
+
+      if (response.statusCode != 200) {
+        throw 'Hay problemas en el servidor, por favor intenelo más tarde.';
+      }
+    } catch (error) {
+      rethrow;
     }
   }
 }
--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -1,27 +1,6 @@
-import 'package:alerta_uaz/data/data_sources/local/storage.dart';
-import 'package:alerta_uaz/domain/model/user_model.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
 abstract class AuthRepository {
-  Future<void> logOutGoogle();
-  Future<GoogleSignInAccount?> logInGoogle();
-  GoogleSignInAccount? getUserGoogle();
-  //User user = User();
-  //Storage storage = Storage('user');
-
-  //Future<void> signIn(String phone);
-  //Future<void> logIn(String? email);
-  //Future<void> logOut();
-
-  //Future<bool> checkUserAuthentication() async {
-    //final isUserAuthenticated = await storage.load();
-
-    // NO hay ningún dato guardado de manera local.
-    //if (isUserAuthenticated == null) return false;
-
-    // Existe, ahora se usara en todo el ciclo de vide de la aplicación.
-    //user.fromJson(isUserAuthenticated);
-
-    //return true;
-  //}
+  Future<void> signIn();
+  Future<void> logIn();
+  Future<void> logOut();
+  Future<bool> checkUserAuthentication();
 }
