bb1636c8c5c3985f6bff4cfcc3d753842afa0870
Luis Adrian Hernandez <luisac658@outlook.com>
2024-12-06 12:51:39 -0600

    Cambios al main page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,16 +1,15 @@
 import 'package:alerta_uaz/application/alert/alert_bloc.dart';
+import 'package:alerta_uaz/application/alert/alert_event.dart';
 import 'package:alerta_uaz/application/alert/alert_state.dart';
 import 'package:alerta_uaz/application/notification/notification_bloc.dart';
 import 'package:alerta_uaz/application/notification/notification_state.dart';
 
-import 'package:alerta_uaz/domain/model/notification_model.dart';
 import 'package:alerta_uaz/presentation/widget/indexed_stack_navigation.dart';
-import 'package:alerta_uaz/presentation/widget/notification_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,24 +39,42 @@ class MainPage extends StatelessWidget {
           },
         ),
         BlocListener<NotificationBloc, NotificationState>(
-          listener: (context, state) {
+          listener: (context, state) async {
             if (state is NotificationReceived) {
-              _showNotificationAlert(context, state.message);
+              final type = state.message.data['type'];
+              final action = await showDialog<bool?>(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text(state.message.notification!.title!),
+                  content: Text(state.message.notification!.body!),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () => Navigator.pop(context, true),
+                      child: const Text('Aceptar'),
+                    ),
+                  ],
+                ),
+              );
+
+              // Solo entrara en caso de ser un ALERT_ACTIVATED
+              if (action == true && type == 'ALERT_ACTIVATED') {
+                final type = state.message.data['type'];
+                if (type == 'ALERT_ACTIVATED') {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    Navigator.of(context).pushReplacementNamed('/map',
+                        arguments: state.message.data['room']);
+                  });
+                }
+              } else if (type == 'ALERT_DESACTIVATED') {
+                final data = state.message.data;
+                // ignore: use_build_context_synchronously
+                context.read<AlertBloc>().add(RegisterContactAlert(data));
+              }
             }
           },
         ),
       ],
       child: const IndexedStackNavigation(),
-    );
-  }
-
-  void _showNotificationAlert(
-      BuildContext context, NotificationMessage message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return NotificationAlert(message: message);
-      },
     );
   }
 }
