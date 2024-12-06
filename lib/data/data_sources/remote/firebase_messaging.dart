6a6d6b8aa5583bb766e202cd077501e7b746f8fb
Luis Adrian Hernandez <luisac658@outlook.com>
2024-12-06 12:25:56 -0600

    Actualización para firebase messaging

---
+++ lib/data/data_sources/remote/firebase_messaging.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:alerta_uaz/firebase_options.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FirebaseMessagingService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Comparte de manera externa las notificaciones que vaya recibiendo.
+  static late StreamController<RemoteMessage> _streamController;
+  static Stream<RemoteMessage> get stream => _streamController.stream;
+
+  static Future<void> initialize() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+
+    await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+  }
+
+  static void setUp() async {
+    _streamController = StreamController<RemoteMessage>.broadcast();
+
+    FirebaseMessaging.onMessage.listen(_handle);
+    FirebaseMessaging.onMessageOpenedApp.listen(_handle);
+  }
+
+  static void _handle(RemoteMessage message) {
+    _streamController.add(message);
+  }
+
+  static Future<String?> getToken() async {
+    return await messaging.getToken();
+  }
+
+  static void deleteToken() async {
+    await messaging.deleteToken();
+  }
+
+  static void refreshToken(Function handler) async {
+    messaging.onTokenRefresh.listen((newToken) => handler);
+  }
+
+  static void streamClose() {
+    _streamController.close();
+  }
+}
