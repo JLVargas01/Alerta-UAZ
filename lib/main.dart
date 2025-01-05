0187c0eb8e19da7edddd8581bc50ad814469e398
Luis Adrian Hernandez <luisac658@outlook.com>
2025-01-05 16:13:59 -0600

    Actualización en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:alerta_uaz/application/alert/alert_bloc.dart';
 import 'package:alerta_uaz/application/authentication/auth_bloc.dart';
 import 'package:alerta_uaz/application/contact/contact_bloc.dart';
-import 'package:alerta_uaz/application/location/location_bloc.dart';
 import 'package:alerta_uaz/application/notification/notification_bloc.dart';
 import 'package:alerta_uaz/application/setting/setting_bloc.dart';
 import 'package:alerta_uaz/data/data_sources/remote/firebase_messaging.dart';
@@ -12,12 +11,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await FirebaseMessagingService.initialize();
   await Location().requestPermission();
+  await Permission.microphone.request();
 
   await dotenv.load();
 
@@ -28,7 +29,6 @@ Future<void> main() async {
         BlocProvider(create: (context) => ContactsBloc()),
         BlocProvider(create: (context) => NotificationBloc()),
         BlocProvider(create: (context) => AlertBloc()),
-        BlocProvider(create: (context) => LocationBloc()),
         BlocProvider(create: (context) => SettingBloc()),
       ],
       child: const AppAlert(),
