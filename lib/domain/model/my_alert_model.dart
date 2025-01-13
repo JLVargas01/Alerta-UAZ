37a0d3e1a933aeef550303d60f1e20f87c7de3ca
Luis Adrian Hernandez <luisac658@outlook.com>
2025-01-12 23:22:42 -0600

    Actualización en el modelo my alert para registrar el audio

--- lib/domain/model/my_alert_model.dart
+++ lib/domain/model/my_alert_model.dart
@@ -9,13 +9,14 @@ class MyAlert {
   double latitude;
   double longitude;
   String date;
-  //Media mediaEnviada
+  String audioPath;
 
   MyAlert({
     required this.uid,
     required this.latitude,
     required this.longitude,
     required this.date,
+    required this.audioPath,
   });
 
   Map<String, Object?> toMap() {
@@ -24,11 +25,12 @@ class MyAlert {
       'latitude': latitude,
       'longitude': longitude,
       'date': date,
+      'audio': audioPath,
     };
   }
 
   @override
   String toString() {
-    return '{latitude: $latitude, longitude: $longitude, date: $date }';
+    return '{latitude: $latitude, longitude: $longitude, date: $date, audio: $audioPath }';
   }
 }
