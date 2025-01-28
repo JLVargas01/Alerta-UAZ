6e38563fb45b479e06a1ee025512766ad447c378
Luis Adrian Hernandez <luisac658@outlook.com>
2025-01-28 02:23:51 -0600

    Actualización en notification api

--- lib/data/data_sources/remote/notification_api.dart
+++ lib/data/data_sources/remote/notification_api.dart
@@ -5,11 +5,15 @@ import 'package:alerta_uaz/core/utils/http_helper.dart';
 
 class NotificationApi {
   // Default: base url = http://localhost:3003/api/notification
-  final String _baseUrl =
-      ApiConfig.getBaseUrl(ApiConfig.portNotification, 'notification');
+  final String _baseUrl = ApiConfig.getBaseUrl(
+    ApiConfig.portNotification,
+    'notification',
+  );
 
-  Future<String> sendNotification(
-      String contactListId, Map<String, Object> message) async {
+  Future<Map<String, dynamic>> sendNotification(
+    String contactListId,
+    Map<String, Object> message,
+  ) async {
     String? endpoint = '/send/$contactListId';
 
     final uri = Uri.parse('$_baseUrl$endpoint');
@@ -20,23 +24,7 @@ class NotificationApi {
 
       final data = json.decode(response.body);
 
-      if (data["success"] > 0 && data['failure'] == 0) {
-        return 'La notificación ha sido enviada exitosamente.';
-      } else if (data["souccess"] > 0 && data['failure'] > 0) {
-        return 'Algunas notificaciones no pudieron ser entregadas.';
-      } else {
-        return 'Hubo problemas al enviar las notificaciones.';
-      }
-
-      // if (response.statusCode == 200) {
-      //   if (data["success"] == 0) {
-      //     throw 'Las notificaciones no pudieron ser entregadas, hay problemas en el servidor.';
-      //   }
-      // } else if (response.statusCode == 404) {
-      //   throw 'No tienes contactos que puedan recibir la alerta.';
-      // } else {
-      //   throw HttpHelper.errorInServer;
-      // }
+      return data;
     } catch (_) {
       rethrow;
     }
