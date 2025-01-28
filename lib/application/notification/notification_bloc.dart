89d6caaf74796b320325dfa606cec26bad09d890
Luis Adrian Hernandez <luisac658@outlook.com>
2025-01-28 03:05:25 -0600

    Actualización de token en notification bloc

--- lib/application/notification/notification_bloc.dart
+++ lib/application/notification/notification_bloc.dart
@@ -1,9 +1,12 @@
 import 'package:alerta_uaz/application/notification/notification_event.dart';
 import 'package:alerta_uaz/application/notification/notification_state.dart';
 import 'package:alerta_uaz/data/data_sources/remote/firebase_messaging.dart';
+import 'package:alerta_uaz/data/data_sources/remote/user_api.dart';
+import 'package:alerta_uaz/domain/model/user_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
+  final UserApi _userApi = UserApi();
   NotificationBloc() : super(NotificationInitial()) {
     on<EnabledNotification>(
       (event, emit) {
@@ -13,6 +16,10 @@ class NotificationBloc extends Bloc<NotificationEvent, NotificationState> {
         final stream = FirebaseMessagingService.stream;
         // Añadimos eventos para cada tipo de notificaiones
         stream.listen((message) => add(ReceiveNotification(message)));
+        // Actualizamos el token automaticamente cada vez que caduca.
+        FirebaseMessagingService.refreshToken((newToken) {
+          _userApi.updateToken(User().id!, newToken);
+        });
       },
     );
     on<DisabledNotification>(
